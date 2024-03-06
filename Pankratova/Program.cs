7780e3f39e0229de6196cb3c9ffe96fa1634bfa1
Zilant1337 <grigorenko_p@inbox.ru>
2024-03-06 14:55:50 +0300

    Fixed coefficient calculations

--- Pankratova/Program.cs
+++ Pankratova/Program.cs
@@ -12,19 +12,19 @@ namespace Pankratova
         static void Main(string[] args)
         {
             double xLeft = 0.001; double xRight=0.2;
-            double eta = 0.0001;
-            List<double> x1 = new List<double>();
-            List<double> x2 = new List<double>();
+            double h = 0.0001;
+            List<double> y1 = new List<double>();
+            List<double> y2 = new List<double>();
             /*List<double> y1Ans = new List<double>();
             List<double> y2Ans = new List<double>();*/
-            x1.Add(2);
-            x2.Add(2);
+            y1.Add(2);
+            y2.Add(2);
             /*
             y1Ans.Add(f1ans(xLeft));
             y2Ans.Add(f2ans(xLeft));
             */
             List<double>xList=new List<double>();
-            for (double x = xLeft; x< xRight; x += eta)
+            for (double eta = xLeft; eta< xRight; eta += h)
             {
                 /* double k11 = f1(y1.Last(),y2.Last(),x);
                  double k21 = f1(y1.Last() + h * k11 / 3, y2.Last() + h * k11 / 3,x + h / 3);
@@ -36,30 +36,36 @@ namespace Pankratova
                  double k32 = f2(y1.Last() - h * k12 / 3 + h * k22, y2.Last() - h * k12 / 3 + h * k22, x + 2 * h / 3);
                  double k42 = f2(y1.Last() + h * k12 - h * k22 + h * k32, y2.Last() + h * k12 - h * k22 + h * k32, x + h);*/
 
-                double k11 = f1(x1.Last(), x2.Last());
-                double k21 = f1(x1.Last() + eta * k11 / 3, x2.Last() + eta * k11 / 3);
-                double k31 = f1(x1.Last() - eta * k11 / 3 + eta * k21, x2.Last() - eta * k11 / 3 + eta * k21);
-                double k41 = f1(x1.Last() + eta * k11 - eta * k21 + eta * k31, x2.Last() + eta * k11 - eta * k21 + eta * k31);
+                double k11 = f1(y1.Last(), y2.Last());
+                double k12 = f2(y1.Last(), eta);
 
-                double k12 = f2(x1.Last(), eta);
-                double k22 = f2(x1.Last() + eta * k12 / 3, eta);
-                double k32 = f2(x1.Last() - eta * k12 / 3 + eta * k22, eta);
-                double k42 = f2(x1.Last() + eta * k12 - eta * k22 + eta * k32, eta);
+                double k21 = f1(y1.Last() + h * k11 / 3, y2.Last() + h * k12 / 3);
+                double k22 = f2(y1.Last() + h * k11 / 3, eta + h / 3);
+                
+                double k31 = f1(y1.Last() - h * k11 / 3 + h * k21, y2.Last() - h * k12 / 3 + h * k22);
+                double k32 = f2(y1.Last() - h * k11 / 3 + h * k21, eta + 2 * h / 3);
+                
+                double k41 = f1(y1.Last() + h * k11 - h * k21 + h * k31, y2.Last() + h * k12 - h * k22 + h * k32);
+                double k42 = f2(y1.Last() + h * k11 - h * k21 + h * k31, eta + h);
 
-                x1.Add(nextY(k11, k21, k31, k41, x1.Last(), eta));
-                x2.Add(nextY(k12, k22, k32, k42, x2.Last(), eta));
+
+
+
+
+                y1.Add(nextY(k11, k21, k31, k41, y1.Last(), h));
+                y2.Add(nextY(k12, k22, k32, k42, y2.Last(), h));
                 /*y1Ans.Add(f1ans(x));
                 y2Ans.Add(f2ans(x));*/
-                xList.Add(x);
+                xList.Add(eta);
             }
             ScottPlot.Plot plot = new ScottPlot.Plot();
-            plot.Add.Scatter(xList.ToArray(),x1.ToArray());
+            plot.Add.Scatter(xList.ToArray(),y1.ToArray());
             /*plot.Add.Scatter(xList.ToArray(), y1Ans.ToArray());*/
-            plot.SavePng("GraphY1.png",2560,1440);
-            plot=new ScottPlot.Plot();
-            plot.Add.Scatter(xList.ToArray(),x2.ToArray());
+            /*plot.SavePng("GraphY1.png",2560,1440);*/
+            /*plot=new ScottPlot.Plot();*/
+            plot.Add.Scatter(xList.ToArray(),y2.ToArray());
             /*plot.Add.Scatter(xList.ToArray(),y2Ans.ToArray());*/
-            plot.SavePng("GraphY2.png", 2560, 1440);
+            plot.SavePng("Graph.png", 2560, 1440);
             /*Console.Write("y1: ");
             foreach(var y in y1)
             {
