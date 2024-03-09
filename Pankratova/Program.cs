d511ff8f92fead34457670d7730265fdb5bd36a5
Zilant1337 <grigorenko_p@inbox.ru>
2024-03-09 23:14:33 +0300

    Added different functions for Serega

--- Pankratova/Program.cs
+++ Pankratova/Program.cs
@@ -11,86 +11,45 @@ namespace Pankratova
     {
         static void Main(string[] args)
         {
-            double xLeft = 0; double xRight=3;
+            double alphaLeft = 1.21922; double alphaRight=1.5;
             double h = 0.0001;
             List<double> y1 = new List<double>();
             List<double> y2 = new List<double>();
-            List<double> y1Ans = new List<double>();
-            List<double> y2Ans = new List<double>();
-            y1.Add(2);
-            y2.Add(2);
+            List<double> y3 = new List<double>();
+            y1.Add(1);
+            y2.Add(1 + alphaLeft);
+            y3.Add(1 + alphaLeft);
 
-            /*y1.Add(f1ans(xLeft));
-            y2.Add(f2ans(xLeft));*/
-            y1Ans.Add(f1ans(xLeft));
-            y2Ans.Add(f2ans(xLeft));
 
             List<double>xList=new List<double>();
-            for (double eta = xLeft; eta< xRight; eta += h)
+            for (double alpha = alphaLeft; alpha< alphaRight; alpha += h)
             {
-                /*double k11 = f1(y1.Last(), y2.Last(), eta);
-                double k12 = f2(y1.Last(), y2.Last(), eta);
+                double k11 = f1(y1.Last(), y2.Last(), y3.Last(), alpha);
+                double k12 = f2(y1.Last(), y2.Last(), y3.Last(), alpha);
+                double k13 = f3(y1.Last(), y2.Last(), y3.Last(), alpha);
 
-                double k21 = f1(y1.Last() + h * k11 / 3, y2.Last() + h * k12 / 3, eta + h / 3);
-                double k22 = f2(y1.Last() + h * k11 / 3, y2.Last() + h * k12 / 3, eta + h / 3);
+                double k21 = f1(y1.Last() + h * k11 / 2, y2.Last() + h * k12 / 2, y3.Last() + h * k13 / 2, alpha + h / 2);
+                double k22 = f2(y1.Last() + h * k11 / 2, y2.Last() + h * k12 / 2, y3.Last() + h * k13 / 2, alpha + h / 2);
+                double k23 = f3(y1.Last() + h * k11 / 2, y2.Last() + h * k12 / 2, y3.Last() + h * k13 / 2, alpha + h / 2);
 
-                double k31 = f1(y1.Last() - h * k11 / 3 + h * k21, y2.Last() - h * k12 / 3 + h * k22, eta + 2 * h / 3);
-                double k32 = f2(y1.Last() - h * k11 / 3 + h * k21, y2.Last() - h * k12 / 3 + h * k22, eta + 2 * h / 3);
+                double k31 = f1(y1.Last() + h * k21/2, y2.Last() + h * k22/2, y3.Last() + h * k23 / 2, alpha + h / 2);
+                double k32 = f2(y1.Last() + h * k21/2, y2.Last() + h * k22/2, y3.Last() + h * k23 / 2, alpha + h / 2);
+                double k33 = f3(y1.Last() + h * k21 / 2, y2.Last() + h * k22 / 2, y3.Last() + h * k23 / 2, alpha + h / 2);
 
-                double k41 = f1(y1.Last() + h * k11 - h * k21 + h * k31, y2.Last() + h * k12 - h * k22 + h * k32, eta + h);
-                double k42 = f2(y1.Last() + h * k11 - h * k12 + h * k31, y2.Last() + h * k12 - h * k22 + h * k32, eta + h);*/
-
-                double k11 = f1(y1.Last(), y2.Last());
-                double k12 = f2(y1.Last(), eta);
-
-                double k21 = f1(y1.Last() + h * k11 / 3, y2.Last() + h * k12 / 3);
-                double k22 = f2(y1.Last() + h * k11 / 3, eta + h / 3);
-
-                double k31 = f1(y1.Last() - h * k11 / 3 + h * k21, y2.Last() - h * k12 / 3 + h * k22);
-                double k32 = f2(y1.Last() - h * k11 / 3 + h * k21, eta + 2 * h / 3);
-
-                double k41 = f1(y1.Last() + h * k11 - h * k21 + h * k31, y2.Last() + h * k12 - h * k22 + h * k32);
-                double k42 = f2(y1.Last() + h * k11 - h * k21 + h * k31, eta + h);
-
-
-
-
+                double k41 = f1(y1.Last() + h * k31, y2.Last() + h * k32, y3.Last() + h * k33, alpha + h);
+                double k42 = f2(y1.Last() + h * k31, y2.Last() + h * k32, y3.Last() + h * k33, alpha + h);
+                double k43 = f3(y1.Last() + h * k31, y2.Last() + h * k32, y3.Last() + h * k33, alpha + h);
 
                 y1.Add(nextY(k11, k21, k31, k41, y1.Last(), h));
                 y2.Add(nextY(k12, k22, k32, k42, y2.Last(), h));
-                y1Ans.Add(f1ans(eta));
-                y2Ans.Add(f2ans(eta));
-                xList.Add(eta);
+                y3.Add(nextY(k13, k13, k13, k13, y3.Last(), h));
+                xList.Add(alpha);
             }
             ScottPlot.Plot plot = new ScottPlot.Plot();
             plot.Add.Scatter(xList.ToArray(),y1.ToArray());
-            /*plot.Add.Scatter(xList.ToArray(), y1Ans.ToArray());*/
-            /*plot.SavePng("GraphY1.png",2560,1440);*/
-            /*plot=new ScottPlot.Plot();*/
             plot.Add.Scatter(xList.ToArray(),y2.ToArray());
-            /*plot.Add.Scatter(xList.ToArray(), y2Ans.ToArray());*/
+            plot.Add.Scatter(xList.ToArray(), y3.ToArray());
             plot.SavePng("Graph.png", 2560, 1440);
-            /*Console.Write("y1: ");
-            foreach(var y in y1)
-            {
-                Console.Write(y.ToString());
-            }
-            Console.Write("\n\nГотовое y1: ");
-            foreach (var y in y1Ans)
-            {
-                Console.Write(y.ToString());
-            }
-            Console.Write("\n\ny2: ");
-            foreach(var y in y2)
-            {
-                Console.Write(y.ToString());
-            }
-            Console.Write("\n\nГотовое y2: ");
-            foreach (var y in y2Ans)
-            {
-                Console.Write(y.ToString());
-            }
-            Console.Read();*/
         }
 
         /*static double f1(double y1, double y2, double x)
@@ -102,25 +61,22 @@ namespace Pankratova
             return y2 - 1 / y1 - Math.Exp(-x);
         }*/
 
-        static double f1(double x1, double x2)
+        static double f1(double y1, double y2,double y3, double alpha)
         {
-            return x2 - (x1 * (Math.Pow(x1, 2) / 3 - 1));
+            return 1+y1*y1*y2-(y3+1)*y1;
         }
-        static double f2(double x1, double eta)
+        static double f2(double y1, double y2, double y3, double alpha)
         {
-            return -eta * x1;
+            return y1*y3-y1*y1*y2;
         }
-        static double f1ans(double x)
+        static double f3(double y1, double y2, double y3, double alpha)
         {
-            return Math.Exp(x);
+            return -y1*y3+alpha;
         }
-        static double f2ans(double x) 
-        {  
-            return Math.Exp(-x); 
-        }
+
         static double nextY(double k1, double k2, double k3, double k4, double prevY,double h)
         {
-            return prevY+h*(k1+3*k2+3*k3+k4)/8;
+            return prevY+h*(k1+2*k2+2*k3+k4)/6;
         }
     }
 }
